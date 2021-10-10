e83dff14b5c3d3bdaa947527d0492b24b10a8d6e
hdpolover <hendrapolover@gmail.com>
2021-10-10 12:31:04 +0700

    up

--- application/controllers/Pendaftaran.php
+++ application/controllers/Pendaftaran.php
@@ -7,7 +7,6 @@ class Pendaftaran extends CI_Controller
     {
         // code...
         parent::__construct();
-        is_logged_in();
         $this->load->library('form_validation');
         $this->load->model('pendaftaran_model', 'pendaftaran');
         $this->load->model('jenjang_model', 'jenjang');
